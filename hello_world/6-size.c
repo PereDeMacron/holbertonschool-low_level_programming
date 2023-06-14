24cf757b26c0eca09bccf2b310f9d7a0871c4e89
PereDeMacron <peredemacron@gmail.com>
2023-06-14 05:27:37 -0700

    task 6 :)

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,7 +6,7 @@
  *    **/
 int main(void)
 {
-			printf("Size of char: %zu byte\n", sizeof(char))
+			printf("Size of char: %zu byte\n", sizeof(char));
 			printf("Size of int: %zu bytes\n", sizeof(int));
 			printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 			printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
