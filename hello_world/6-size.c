96ecd5e8c829de94e460559cb57227061986b444
PereDeMacron <peredemacron@gmail.com>
2023-06-14 05:14:43 -0700

    task 6 :)

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,7 +8,9 @@ int main(void)
 {
 			printf("Size of int: %zu bytes\n", sizeof(int));
 			printf("Size of float: %zu bytes\n", sizeof(float));
-			printf("Size of double: %zu bytes\n", sizeof(double));
+			printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+			printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 			printf("Size of char: %zu byte\n", sizeof(char));
+			
 			return (0);
 }
