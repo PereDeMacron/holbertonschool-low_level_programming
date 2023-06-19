5b18ef97d9daebdd9d9200af401a0bec408ff160
PereDeMacron <peredemacron@gmail.com>
2023-06-19 02:49:05 -0700

    new tasks

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,6 @@
 int main(void)
 {
 	int n;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
@@ -15,6 +14,5 @@ int main(void)
 		printf("%d is negative\n", n);
 	else
 		printf("%d is zero\n", n);
-
 	return (0);
 }
