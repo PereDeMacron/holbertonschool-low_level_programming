121785d160813bcafe671896031855c3479b8ced
PereDeMacron <peredemacron@gmail.com>
2023-06-19 02:46:32 -0700

    new tasks

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,29 +1,14 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+/* more headers goes there */
 
+/* betty style doc for function main goes there */
 int main(void)
 {
-    int n;
+	int n;
 
-    srand(time(NULL));
-    n = rand() - RAND_MAX / 2;
-
-    printf("The string Last digit of %d is ", n);
-
-    int lastDigit = abs(n % 10);
-    if (lastDigit > 5)
-    {
-        printf("and is greater than 5\n");
-    }
-    else if (lastDigit == 0)
-    {
-        printf("and is 0\n");
-    }
-    else
-    {
-        printf("and is less than 6 and not 0\n");
-    }
-
-    return 0;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	return (0);
 }
