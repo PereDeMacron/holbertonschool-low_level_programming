30d0b0733cd781f86b5b55e722e6589ab8cd02eb
PereDeMacron <peredemacron@gmail.com>
2023-06-23 05:00:34 -0700

    more nested loop

---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * Auth: Gary M
+ * Desc: Header file containing prototypes for all functions
+ *       used in the taskl more_functions_nested_loops.
+ */
+
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
