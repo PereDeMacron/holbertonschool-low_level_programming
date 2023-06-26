6f1843f6bb301f3ef43c68b7f8ab19eef5524a57
PereDeMacron <peredemacron@gmail.com>
2023-06-26 01:13:50 -0700

    task 2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *
+ * _strlen - returns the length of a string
+ *
+ */
+
+size_t _strlen(const char *str)
+{
+		size_t length = 0;
+
+			while (*str++)
+						length++;
+
+				return (length);
+}
