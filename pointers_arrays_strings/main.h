bcf635a07e268aa834817be73be86e03d41c4b92
PereDeMacron <peredemacron@gmail.com>
2023-06-26 01:00:17 -0700

    add readme + task 0

---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ * Auth: Gary.M
+ * Desc: Hfile contain prototypes for all functions
+ *       used in task pointers_arrays_strings.
+ */
+
+#include <stdlib.h>
+
+int _putchar(char c);
+void reset_to_98(int *n);
+
+#endif
