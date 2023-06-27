e3794d0357fd689bb723b26efea37d569dece5c1
PereDeMacron <peredemacron@gmail.com>
2023-06-27 01:28:56 -0700

    task 2.3 & 2.4

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+void reverse_array(int *a, int n)
+{
+	int index = 0;
+	
+	while (str[index])
+		{
+		if (str[index] >= 'a' && str[index] <= 'z')
+		str[index] -= 32;
+
+		index++;
+		}
+
+	return (str);
+}
