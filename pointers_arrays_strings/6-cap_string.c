63f0a545c1dea1e8a5231a03a81fec7b0d92aa0b
PereDeMacron <peredemacron@gmail.com>
2023-06-27 05:45:54 -0700

    task 6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,33 +1,28 @@
 #include "main.h"
 
-char *cap_string(char *str)
+char *cap_string(char *s)
 {
-	int index = 0;
+	int i, j;
 
-	while (str[index])
+	char sep[13] = {' ', '\t', '\n', ',', ';', '.',
+			'!', '?', '"', '(', ')', '{', '}'};
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		while (!(str[index] >= 'a' && str[index] <= 'z'))
-		index++;
-
-		if (str[index - 1] == ' ' ||
-		str[index - 1] == '\t' ||
-		str[index - 1] == '\n' ||
-		str[index - 1] == ',' ||
-		str[index - 1] == ';' ||
-		str[index - 1] == '.' ||
-		str[index - 1] == '!' ||
-		str[index - 1] == '?' ||
-		str[index - 1] == '"' ||
-		str[index - 1] == '(' ||
-		str[index - 1] == ')  ||
-		str[index - 1] == '{' ||
-		str[index - 1] == '}' ||
-		index == 0)	
-		
-		str[index] -= 32;
-
-		index++;
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 32;
+		}	
+		for (j = 0; j <= 13; j++)
+		{
+			if (s[i] == sep[j])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				{
+					s[i + 1] = s[i + 1] - 32;
+				}
+			}
+		}
 	}
-
-	return (str);
+	return (s);
 }
