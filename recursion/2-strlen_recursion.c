083aa87c11a26fd81bf593c470157f8254b0d0eb
PereDeMacron <peredemacron@gmail.com>
2023-07-03 02:26:59 -0700

    add all task at once

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,13 +1,21 @@
 #include "main.h"
 
+/**
+ * _strlen_recursion - Calculates the length of a string recursively.
+ * @s: Pointer to the string.
+ *
+ * Return: The length of the string.
+ */
 int _strlen_recursion(char *s)
 {
 	int len = 0;
-	if (*s)
 
+	if (*s)
 	{
 		len++;
 		len += _strlen_recursion(s + 1);
 	}
+
 	return (len);
 }
+
