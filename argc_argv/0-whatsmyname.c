0d82dfbab6b300e6bb7441a926aab3879ba8132e
PereDeMacron <peredemacron@gmail.com>
2023-07-04 01:55:42 -0700

    correct betty style

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
 /**
- * FileName: 0-whatsmyname.c
- * Desc: print the name of the program
- * Auth: PDM
+ * main - Entry point of the program
+ * @argc: The number of command-line arguments
+ * @argv: An array of command-line argument strings
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
+	    printf("%s\n", argv[0]);
 
-	return (0);
+	        return (0);
 }
