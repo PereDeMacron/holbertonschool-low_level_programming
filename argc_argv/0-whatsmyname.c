4fe45759a8a1d2929ea95e1baf7c3d84b79d6023
PereDeMacron <peredemacron@gmail.com>
2023-07-04 01:51:05 -0700

    correct betty style

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+/**
+ * FileName: 0-whatsmyname.c
+ * Desc: print the name of the program
+ * Auth: PDM
+ */
+
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	printf("%s\n", argv [0]);
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
