9e6781bfbee1318d7856dfd07114caf90d4122f5
PereDeMacron <peredemacron@gmail.com>
2023-07-11 04:28:18 -0700

    all task

---
+++ preprocessor/0-object_like_macro.h
@@ -0,0 +1,13 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/**
+ * File: 0-object_like_macro.h
+ * Desc: Define SIZE at 1024
+ *
+ *
+ */
+
+#define SIZE 1024
+
+#endif
---
+++ preprocessor/4-sum.h
@@ -0,0 +1,11 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ * File: 4-sum.h
+ * Desc: define a function like a macro
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
