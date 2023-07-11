9dcf349824fe4ea9712250cc68db87608a054fef
PereDeMacron <peredemacron@gmail.com>
2023-07-11 01:44:18 -0700

    add all task with betty style

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * string_nconcat - concatenates two string
+ *
+ * @s1: first str
+ * @s2: second str
+ * @n: sddjfizejfizjfi
+ * Return: On success, returns On failure or if s1 or s2 is NULL, return NULL.
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *concat;
+	unsigned int len = n, index;
+
+	if (s1 == NULL)
+	s1 = "";
+
+	if (s2 == NULL)
+	s2 = "";
+
+	for (index = 0; s1[index] || s2[index]; index++)
+	len++;
+
+	concat = malloc(sizeof(char) * (len + 1));
+
+	if (concat == NULL)
+	return (NULL);
+
+	len = 0;
+
+	for (index = 0; s1[index]; index++)
+	concat[len++] = s1[index];
+
+	for (index = 0; s2[index]; index++)
+	concat[len++] = s2[index];
+
+	concat[len] = '\0';
+
+	return (concat);
+}
