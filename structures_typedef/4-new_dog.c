9c95a87f8ce3e27238ea9f68881909ee85efa3c3
PereDeMacron <peredemacron@gmail.com>
2023-07-17 05:36:14 -0700

    add all task without betty doc style

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,38 @@
+#include "dog.h"
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog_ptr;
+	char *name_copy, *owner_copy;
+	int name_length, owner_length;
+
+	name_length = strlen(name);
+	owner_length = strlen(owner);
+
+	new_dog_ptr = malloc(sizeof(dog_t));
+	if (new_dog_ptr == NULL)
+	return (NULL);
+
+	name_copy = malloc(name_length + 1);
+	if (name_copy == NULL)
+	{
+		free(new_dog_ptr);
+		return (NULL);
+	}
+	strcpy(name_copy, name);
+
+	owner_copy = malloc(owner_length + 1);
+	if (owner_copy == NULL)
+	{
+		free(name_copy);
+		free(new_dog_ptr);
+		return (NULL);
+	}
+	strcpy(owner_copy, owner);
+
+	new_dog_ptr->name = name_copy;
+	new_dog_ptr->age = age;
+	new_dog_ptr->owner = owner_copy;
+
+	return (new_dog_ptr);
+}
