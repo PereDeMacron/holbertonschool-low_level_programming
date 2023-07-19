ddb15626dfbd19c4f8cec018f2e0fef71926519c
PereDeMacron <peredemacron@gmail.com>
2023-07-19 01:17:18 -0700

    task 0/1/2 without betty style

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -2,15 +2,14 @@
 #include <stdarg.h>
 
 int sum_them_all(const unsigned int n, ...) {
-	if (n == 0) {
-		return 0;
-	}
 
 	unsigned int i;
 	int sum = 0;
 	va_list args;
 	va_start(args, n);
-
+	if (n == 0) {
+		return 0;
+	}
 	for (i = 0; i < n; i++) {
 		sum += va_arg(args, int);
 	}
--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -1,10 +1,10 @@
 #include "variadic_functions.h"
 
 void print_numbers(const char *separator, const unsigned int n, ...) {
+	unsigned int i;
 	va_list args;
 	va_start(args, n);
 
-	unsigned int i;
 	for (i = 0; i < n; ++i) {
 		printf("%d", va_arg(args, int));
 
