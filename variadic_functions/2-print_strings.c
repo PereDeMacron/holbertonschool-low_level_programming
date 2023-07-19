800a803129c31d2589dff372517502002e271dc4
PereDeMacron <peredemacron@gmail.com>
2023-07-19 02:09:52 -0700

    add betty styl on task 0/1/2

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -1,20 +1,30 @@
 #include "variadic_functions.h"
 
-void print_strings(const char *separator, const unsigned int n, ...) {
+/**
+ * print_strings -  prints strings, followed by a new line
+ *
+ * @separator: .
+ * @n: .........
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
 	unsigned int i;
 	va_list args;
+
 	va_start(args, n);
 
-	for (i = 0; i < n; ++i) {
+	for (i = 0; i < n; ++i)
+	{
 		char *str = va_arg(args, char *);
-		if (str != NULL) {
+
+		if (str != NULL)
 			printf("%s", str);
-		} else {
+		else
 			printf("(nil)");
-		}
-		if (separator != NULL && i < n - 1) {
+
+		if (separator != NULL && i < n - 1)
 			printf("%s", separator);
-		}
 	}
 	va_end(args);
 	printf("\n");
