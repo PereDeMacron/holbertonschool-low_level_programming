b11ae45e1aa2759ae9b2dc68888d05dd84079719
PereDeMacron <peredemacron@gmail.com>
2023-08-01 14:14:27 -0700

    all tasks 0/1/2/3/4

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+list_t *add_node(list_t **head, const char *str)
+{
+    char *dup;
+    list_t *new;
+
+    new = malloc(sizeof(list_t));
+    if (new == NULL)
+        return (NULL);
+
+    dup = strdup(str);
+    if (dup == NULL)
+    {
+        free(new);
+        return (NULL);
+    }
+
+    new->str = dup;
+    new->len = strlen(str);
+    new->next = *head;
+
+    *head = new;
+
+    return (new);
+}
