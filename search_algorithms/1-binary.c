75a8e9f503deafefe238c36ae598b078a2de05ca
PereDeMacron <peredemacron@gmail.com>
2023-09-20 09:48:07 +0200

    betty fix

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -5,35 +5,35 @@
  * @array: Pointer the first array element
  * @size: size of the array
  * @value: value to find
- * 
+ *
  * Return: where the value is located or -1 if it not found
 */
 int binary_search(int *array, size_t size, int value)
 {
-    size_t mid, i;
-    int result;
+	size_t mid, i;
+	int result;
 
-    if (array == NULL || size == 0)
-        return -1;
+	if (array == NULL || size == 0)
+		return (-1);
 
-    printf("Searching in array: %d", array[0]);
-    for (i = 1; i < size; i++) {
-        printf(", %d", array[i]);
-    }
-    printf("\n");
+	printf("Searching in array: %d", array[0]);
+	for (i = 1; i < size; i++)
+		printf(", %d", array[i]);
 
-    mid = size / 2;
+	printf("\n");
 
-    if (array[mid] == value)
-        return mid;
+	mid = size / 2;
 
-    if (array[mid] > value)
-        return binary_search(array, mid, value);
+	if (array[mid] == value)
+		return (mid);
 
-    result = binary_search(array + mid + 1, size - mid - 1, value);
+	if (array[mid] > value)
+		return (binary_search(array, mid, value));
 
-    if (result != -1)
-        return mid + 1 + result;
+	result = binary_search(array + mid + 1, size - mid - 1, value);
 
-    return -1;
+	if (result != -1)
+		return (mid + 1 + result);
+
+	return (-1);
 }
